9ecc379161d41f6a92f2dc7f61ed5f364097f7dc
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-07-17 12:16:30 +0100

    feat: toggle theming

--- src/ThemeToggler/ThemeToggler.jsx
+++ src/ThemeToggler/ThemeToggler.jsx
@@ -6,8 +6,23 @@ import styles from "./ThemeToggler.module.css";
 export default function ThemeToggler({ setTheme }) {
   const theme = useContext(ThemeContext);
 
+  let togglerClass = styles.toggler;
+
+  switch (theme) {
+    case 1:
+      togglerClass += ` ${styles.toggler_th1}`;
+      break;
+    case 2:
+      togglerClass += ` ${styles.toggler_th2}`;
+      break;
+    case 3:
+    default:
+      togglerClass += ` ${styles.toggler_th3}`;
+      break;
+  }
+
   return (
-    <div className={styles.toggler}>
+    <div className={togglerClass}>
       <ul className={`${styles.toggler__themeList} fs-small`} role="list">
         {themes.map((theme, i) => (
           <li key={theme} title={theme}>
